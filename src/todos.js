70a77f5604ff061afbef6c4de762181d7209388b
Isabella Harrington <isabellaharrington@gmail.com>
2025-08-31 16:36:56 -0400

    Created basic todos.js file containing an object constructor for creating basic todo list items

---
+++ src/todos.js
@@ -0,0 +1,8 @@
+function Todo(title, desc, dueDate, priority) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+}
+
+export default Todo;
